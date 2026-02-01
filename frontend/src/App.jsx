651c4723fde986c88138105ec82ca13bf76b8f2e
Aditya Narayan Mohanty <adimohanty2001@gmail.com>
2026-02-02 00:48:31 +0530

    Add React Router and route all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
-import ExamSelection from "./pages/ExamSelection";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
+import ExamSelection from "./pages/ExamSelection";
 import CategorySelection from "./pages/CategorySelection";
 import Instructions from "./pages/Instructions";
 import Quiz from "./pages/Quiz";
@@ -7,17 +9,27 @@ import Result from "./pages/Result";
 import Review from "./pages/Review";
 import Admin from "./pages/Admin";
 
-function App() {
+import DevNavBar from "./components/DevNavBar";
+
+export default function App() {
   return (
-    // <Login />
-    // <ExamSelection />
-    // <CategorySelection />
-    // <Instructions />
-    // <Quiz />
-    // <Result />
-    // <Review />
-    <Admin />
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/exams" element={<ExamSelection />} />
+        <Route path="/categories" element={<CategorySelection />} />
+        <Route path="/instructions" element={<Instructions />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/review" element={<Review />} />
+
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+
+      {/* 🔧 Temporary Dev Navigation */}
+      <DevNavBar />
+    </>
   );
 }
-
-export default App;
